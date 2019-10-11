fe106780de7b956716fb6b7de07ee1885279347a
Mauricio Mutte <litiobr@gmail.com>
2019-10-10 21:22:55 -0300

    feat: add content and style

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.text, styles.title]}>Estamos juntos há:</Text>
+      <Text style={[styles.text, styles.days]}>
+        587
+        <Text style={[styles.text, styles.small]}>dias</Text>
+      </Text>
     </View>
   );
 }
@@ -12,8 +16,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B7004E',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 30,
   },
+
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 38,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+
+  title: {
+    textShadowColor: 'rgba(0,0,0,.7)',
+    textShadowRadius: 10
+  },
+
+  small: {
+    fontSize: 22
+  },
+
+  days: {
+    fontSize: 60,
+    textShadowColor: '#000',
+    textShadowOffset: {width: 1, height: 1},
+    textShadowRadius: 1
+  }
 });
